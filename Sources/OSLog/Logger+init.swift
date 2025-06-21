387ecb44b906a41329c32e5747f257763b15a587
rrroyal <royal@shameful.xyz>
2025-06-21 11:52:19 +0200

    [OSLog] Add additional initializer

--- Sources/OSLog/Logger+init.swift
+++ Sources/OSLog/Logger+init.swift
@@ -5,15 +5,23 @@
 //  Created by royal on 26/12/2022.
 //
 
-import OSLog
+import CommonFoundation
+public import OSLog
 
 // MARK: - Logger+init
 
 public extension Logger {
 	/// Convenience initializer with `subsystem` already filled in.
-	/// - Parameter category: Category of `Logger`
+	/// - Parameter category: Category of `Logger`.
 	@inlinable
 	init(category: String) {
-		self.init(subsystem: Bundle.main.bundleIdentifier ?? "", category: category)
+		self.init(subsystem: Bundle.main.mainBundleIdentifier ?? Bundle.main.bundleIdentifier ?? "", category: category)
+	}
+
+	/// Convenience initializer with `subsystem` already filled in.
+	/// - Parameter category: Category of `Logger`.
+	@inlinable
+	init(category: Any.Type) {
+		self.init(category: "\(category)")
 	}
 }
